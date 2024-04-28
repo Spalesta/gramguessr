5db1aa816e4eaaff3e75f06feffa99bd08f38d56
Spalesta <spalesta@gmail.com>
2024-04-28 14:20:14 +0300

    добавил токен бота

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-botTimeWeb = telebot.TeleBot('Уникальный токен')
+botTimeWeb = telebot.TeleBot('7133097481:AAGizdgJ2SPCFJb6KynnTn_k5ilPfbKbwLY')
 
 from telebot import types
 
@@ -11,3 +11,11 @@ def startBot(message):
   button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
   markup.add(button_yes)
   botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+  
+
+'''from pygrambank import Grambank
+gb = Grambank('.')
+gb.sheets_dir
+for sheet in gb.iter_sheets():
+   print(sheet)
+   break'''
