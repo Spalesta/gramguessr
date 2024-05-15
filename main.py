69d7b250a895cc8cc6bdddbc8fc7b8351503f5d3
Spalesta <spalesta@gmail.com>
2024-05-15 22:43:16 +0300

    почему после "начать игру" он снова присылает "давайте начнем"...

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ def get_text_messages(message):
     global currently_studying
     if message.text == facl_list or most_list:
         if message.text == facl_list:
-            currently_studying[message.from_user.id] = [facl, 0]
+            currently_studying[message.from_user.id] = [random.sample(facl, len(facl)), 0]
         elif message.text == most_list:
-            currently_studying[message.from_user.id] = [most, 0]
+            currently_studying[message.from_user.id] = [random.sample(most, len(most)), 0]
         markup = types.ReplyKeyboardMarkup()
         btn1 = types.KeyboardButton('начать игру')
         markup.add(btn1)
         bot.send_message(message.from_user.id, f'давайте начнем {currently_studying[message.from_user.id][0]}', reply_markup=markup)  # ответ бота
-    if message.text == 'начать игру':
+    elif message.text == 'начать игру':
         markup = types.ReplyKeyboardMarkup()
         for lang in currently_studying[message.from_user.id][0]:
             markup.add(types.KeyboardButton(lang))
-        bot.send_message(message.from_user.id, facl[0], reply_markup=markup)  # ответ бота
+        bot.send_message(message.from_user.id, facl[0], reply_markup=markup)
 
 
-import csv
+'''import csv
 def choose_language(li):
     random.shuffle(li)
     lang = li[0].capitalize()
@@ -44,7 +44,7 @@ def choose_language(li):
     with open('data/draft.csv') as raw_data:
         reader = list(csv.reader(raw_data, delimiter=','))
         lang_data = [(line[2], line[4]) for line in reader if line[1] == lang]
-        return lang_data
+        return lang_data'''
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
 
