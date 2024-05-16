d0c1f0117caac3e145e9b00df812e74c1eea6f16
Spalesta <spalesta@gmail.com>
2024-05-16 16:00:59 +0300

    fixed some pep-8 stuff

--- main.py
+++ main.py
@@ -1,45 +1,50 @@
 import telebot
+from telebot import types
+import langlists
+import random
 
 bot = telebot.TeleBot('7133097481:AAGizdgJ2SPCFJb6KynnTn_k5ilPfbKbwLY')
 
-from telebot import types
-from langlists import most, facl
-import random
+
 facl_list = "фикл"
 most_list = "самые изучаемые"
+
+
 @bot.message_handler(commands=['start'])
-def startBot(message):
+def startbot(message):
     markup = types.ReplyKeyboardMarkup()
     btn1 = types.KeyboardButton(facl_list)
     btn2 = types.KeyboardButton(most_list)
     markup.add(btn1, btn2)
     bot.send_message(message.from_user.id, "Выберите языки", reply_markup=markup)
 
+
 currently_studying = {'userid': [['languages'], 0]}
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global currently_studying
     if message.text == facl_list or message.text == most_list:
         if message.text == facl_list:
-            currently_studying[message.from_user.id] = [random.sample(facl, len(facl)), 0]
+            currently_studying[message.from_user.id] = [random.sample(langlists.facl, len(langlists.facl)), 0]
         elif message.text == most_list:
-            currently_studying[message.from_user.id] = [random.sample(most, len(most)), 0]
+            currently_studying[message.from_user.id] = [random.sample(langlists.most, len(langlists.most)), 0]
         markup = types.ReplyKeyboardMarkup()
         btn1 = types.KeyboardButton('начать игру')
         markup.add(btn1)
-        bot.send_message(message.from_user.id, f'давайте начнем {currently_studying[message.from_user.id][0]}', reply_markup=markup)  # ответ бота
+        bot.send_message(message.from_user.id, f'давайте начнем {currently_studying[message.from_user.id][0]}',
+                         reply_markup=markup)  # ответ бота
     elif message.text == 'начать игру':
         markup = types.ReplyKeyboardMarkup()
         for lang in currently_studying[message.from_user.id][0]:
             print(lang)
             markup.add(types.KeyboardButton(lang))
-        bot.send_message(message.from_user.id, facl[0], reply_markup=markup)
-    elif message.text in facl + most:
+        bot.send_message(message.from_user.id, langlists.facl[0], reply_markup=markup)
+    elif message.text in langlists.facl + langlists.most:
         bot.send_message(message.from_user.id, 'неправильно вы лох', reply_markup=None)
     else:
         bot.send_message(message.from_user.id, 'что?????', reply_markup=None)
-
 
 
 '''import csv
@@ -52,7 +57,7 @@ def choose_language(li):
         lang_data = [(line[2], line[4]) for line in reader if line[1] == lang]
         return lang_data'''
 
-bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
+bot.polling(none_stop=True, interval=0)  # обязательная для работы бота часть
 
 '''
 python main.py
